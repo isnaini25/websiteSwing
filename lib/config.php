dcd39e197b8e09aaf5c78239f0866e596dadc315
leeseona25 <isnaini.25@sutdents.amikom.ac.id>
2021-01-10 15:40:15 +0700

    Update Projek

--- lib/config.php
+++ lib/config.php
@@ -1,6 +1,6 @@
 <?php 
 $base_url = "https://swing-jahit.herokuapp.com/";
-$admin_url = "https://swing-jahit.herokuapp.com/";
+$admin_url = "https://swing-jahit.herokuapp.com/admin/";
 // $base_url = "http://localhost/swing/";
 // $admin_url = "http://localhost/swing/admin/";
  ?>
