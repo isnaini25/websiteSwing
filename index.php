9fec1ed21176aa53ebd521d749e5c83a2b7c36e8
Isnaini Solikhah <isnaini.25@students.amikom.ac.id>
2023-08-28 21:08:18 +0700

    Update navbar margin

--- index.php
+++ index.php
@@ -15,37 +15,43 @@
     <link rel="stylesheet" href="assets/css/jquery.timepicker.css">
 
     <script type="text/javascript " src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js "></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js " integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo " crossorigin="anonymous "></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js "
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo " crossorigin="anonymous ">
+    </script>
     <script src="assets/js/bootstrap.bundle.js"></script>
-    
-    
-   
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Abel&family=Bebas+Neue&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Abel&family=Bebas+Neue&family=Roboto:wght@300;400;700&display=swap"
+        rel="stylesheet">
     <title>SWING</title>
 
 
 </head>
 <style>
-    body {
-        background-color: rgb(233, 233, 233);
-        padding: 0;
-        margin: 0;
-    }
+body {
+    background-color: rgb(233, 233, 233);
+    padding: 0;
+    margin: 0;
+}
 </style>
 
 <body>
     <header>
         <div class="container-fluid p-0 jumbotron">
             <div class="overlay">
-                
-                <nav class="navbar navbar-expand-lg navbar-dark mr-5">
+
+                <nav class="navbar navbar-expand-lg navbar-dark mr-md-5">
                     <img src="assets/img/logo-compact.png" alt="Logo" id="logo">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
@@ -71,10 +77,15 @@
                 <div class="jumbotron-content">
                     <div class="jumbotron-text">
                         <h1>SWING</h1>
-                        <p> Swing adalah e-marketplace jasa jahit berbasis website yang digunakan untuk melakukan pemesanan jasa
-                            <br> penjahit secara online. Swing mempertemukan penjahit dan konsumen yang membutuhkan jasa penjahit.
-                            <br> Para penjahit dapat mendaftarkan jasanya di e-marketplace ini dan konsumen dapat mencari jasa penjahit
-                            <br>tanpa harus datang ke tempat penjahit. Swing memudahkan konsumen dalam melakukan transaksi jasa jahit.</p>
+                        <p> Swing adalah e-marketplace jasa jahit berbasis website yang digunakan untuk melakukan
+                            pemesanan jasa
+                            <br> penjahit secara online. Swing mempertemukan penjahit dan konsumen yang membutuhkan jasa
+                            penjahit.
+                            <br> Para penjahit dapat mendaftarkan jasanya di e-marketplace ini dan konsumen dapat
+                            mencari jasa penjahit
+                            <br>tanpa harus datang ke tempat penjahit. Swing memudahkan konsumen dalam melakukan
+                            transaksi jasa jahit.
+                        </p>
                     </div>
 
                     <div class="jumbotron-button">
@@ -99,14 +110,14 @@
                 include "lib/koneksi.php";
                 $kueri = mysqli_query($koneksi, "SELECT * FROM tbl_penjahit LIMIT 8");
                 while ($res = mysqli_fetch_array($kueri)) { ?>
-                    <div class="col-lg-3 col-md-3 col-sm-6">
-                        <div class="penjahit-item">
-                            <div class="penjahit-gambar">
-                                <img src="admin/upload/<?php echo $res['foto']; ?>" alt="">
-                            </div>
-                            <div class="penjahit-desc">
-                                <span class="penjahit-name"><?php echo $res['nama']; ?></span>
-                                <?php
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <div class="penjahit-item">
+                        <div class="penjahit-gambar">
+                            <img src="admin/upload/<?php echo $res['foto']; ?>" alt="">
+                        </div>
+                        <div class="penjahit-desc">
+                            <span class="penjahit-name"><?php echo $res['nama']; ?></span>
+                            <?php
                                 $idPenjahit = $res['idPenjahit'];
                                 $kueriItem = mysqli_query($koneksi, "SELECT * FROM tbl_itemKategori a INNER JOIN tbl_kategori b ON a.idKategori = b.idKategori WHERE idPenjahit = '$idPenjahit' LIMIT 2");
                                 $no = 0;
@@ -114,22 +125,22 @@
 
                                 ?>
 
-                                    <span class="penjahit-category">
-                                        <?php if ($no > 0) {
+                            <span class="penjahit-category">
+                                <?php if ($no > 0) {
                                             echo ", ";
                                         }
                                         echo $resItem['namaKategori']; ?>
-                                    <?php $no++;
+                                <?php $no++;
                                 } ?>
-                                    </span>
-
-                            </div>
-                            <div class="next">
-                                <a href="profilPenjahit.php?id=<?php echo $res['idPenjahit']; ?>">LIHAT </a>
-                            </div>
+                            </span>
 
                         </div>
+                        <div class="next">
+                            <a href="profilPenjahit.php?id=<?php echo $res['idPenjahit']; ?>">LIHAT </a>
+                        </div>
+
                     </div>
+                </div>
                 <?php } ?>
 
 
@@ -147,7 +158,7 @@
                     <div class="row">
                         <div class="col-sm-12 info-content">
                             <div class="info-gambar">
-                                <img src="assets/img/stroberydress.png " alt="dress " width="300px" height="330px" >
+                                <img src="assets/img/stroberydress.png " alt="dress " width="300px" height="330px">
                             </div>
                             <!-- Masih gak responsive cari pemecahannya! -->
                             <div class="info-teks">
@@ -160,12 +171,13 @@
                                 <div class="info-plus">
                                     <div class="plus-content">
                                         <i class="fa fa-heart" aria-hidden="true"></i>
-                                        <p>Kepuasan pelanggan sangat diutamakan. Kamu dijamin puas dengan hasil pesanan jahitmu.
+                                        <p>Kepuasan pelanggan sangat diutamakan. Kamu dijamin puas dengan hasil pesanan
+                                            jahitmu.
                                         </p>
                                     </div>
                                     <div class="plus-content">
                                         <i class="fas fa-comment"></i>
-                                        <p>Kamu bisa berdiskusi dengan penjahit mengenai pesanan jahitmu.  
+                                        <p>Kamu bisa berdiskusi dengan penjahit mengenai pesanan jahitmu.
                                         </p>
                                     </div>
                                     <div class="plus-content">
@@ -186,7 +198,7 @@
 
     </main>
 
-   <?php 
+    <?php 
    include "templates/footer.php";
    ?>
 
